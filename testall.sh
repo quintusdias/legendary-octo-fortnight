ef7c774b53112007eb92675ea3b7129b8e9e67c4
John Evans <john.g.evans.ne@gmail.com>
2016-02-27 13:22:00 -0500

    Add test shell script
    Crawl the filesystem looking for PDFs.

---
+++ testall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+space_regex='[[:space:]]'
+
+locate "*.pdf" | while read pdf
+do
+	if [ ! -f "$pdf" ]
+	then
+		echo skipping "$pdf" it is not really there
+		continue
+	fi
+	fs=$(ls -al "$pdf" | awk '{print $5}')
+	if (( fs > 20000 ))
+	then
+		echo skipping "$pdf"
+		continue
+	fi
+	if [[ "$pdf" =~ $space_regex ]]
+	then
+		echo skipping "$pdf"
+		continue
+	fi
+	printf "%s\t%s\n" "$fs" "\"$pdf\""
+	spdfinfo -i "$pdf"
+done
